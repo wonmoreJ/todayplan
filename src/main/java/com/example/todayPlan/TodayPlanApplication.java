138d0d0ee97dd898edfbb766904ede412ab53b61
onemong <booboo929292@gmail.com>
2025-04-05 01:53:23 +0900

    ignore부분 수정

--- src/main/java/com/example/todayPlan/TodayPlanApplication.java
+++ src/main/java/com/example/todayPlan/TodayPlanApplication.java
@@ -9,13 +9,13 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class TodayPlanApplication {
 
 	public static void main(String[] args) {
-		 // .env 로드
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-
-        // Spring 환경변수로 등록
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		/*
+		 * // .env 로드 Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		 * 
+		 * // Spring 환경변수로 등록 System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		 * System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		 * System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		 */
 		SpringApplication.run(TodayPlanApplication.class, args);
 	}
 
